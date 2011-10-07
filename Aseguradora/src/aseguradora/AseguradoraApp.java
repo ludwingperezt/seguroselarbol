7f3b9f2277d681459b48d31d49c42498057d04a3
mirage232 <mirage232@e827bc52-037f-11bd-7ced-47b2804041a0>
2011-10-07 06:50:33 +0000

    base

---
+++ Aseguradora/src/aseguradora/AseguradoraApp.java
@@ -0,0 +1,44 @@
+/*
+ * AseguradoraApp.java
+ */
+
+package aseguradora;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class AseguradoraApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new AseguradoraView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of AseguradoraApp
+     */
+    public static AseguradoraApp getApplication() {
+        return Application.getInstance(AseguradoraApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(AseguradoraApp.class, args);
+    }
+}
